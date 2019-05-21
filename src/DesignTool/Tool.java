1baf7bc83e772035f5e65b8d344890fdedf03cfb
PI\Pierce <pierceevans4@gmail.com>
2019-05-21 16:39:03 +1000

    Added Colour Panel and colour Buttons underneath

--- src/DesignTool/Tool.java
+++ src/DesignTool/Tool.java
@@ -34,7 +34,6 @@ public class Tool extends JFrame implements ActionListener{
     private JButton btnMagenta;
     private JButton btnPink;
     private JButton btnBlack;
-    private JButton btnDarkGrey;
     private JButton btnGrey;
     private JButton btnLightGrey;
     private JButton btnWhite;
@@ -84,12 +83,12 @@ public class Tool extends JFrame implements ActionListener{
     public void setupPanels() {
         pnlMain = new JPanel(frame.getLayout());
         pnlMain.setBackground(Color.WHITE);
-        pnlColour = new JPanel(new GridLayout(9, 2, 5, 5));
-        pnlColour.setBackground(Color.lightGray);
-        pnlColour.setBounds(0, HEIGHT - 350, WIDTH, 350);
+        pnlColour = new JPanel(new GridLayout(2, 8, 5, 5));
+        pnlColour.setBackground(new Color(255, 255, 200));
+        pnlColour.setBounds(0, HEIGHT - 100, WIDTH, 100);
         pnlMenu = new JPanel(new GridLayout(10, 1, 3, 3));
         pnlMenu.setBackground(Color.RED);
-        pnlMenu.setBounds(10, 150, 100, 150);
+        pnlMenu.setBounds(0, 150, 100, 150);
         frame.add(pnlColour, BorderLayout.PAGE_END);
         frame.add(pnlMain, BorderLayout.CENTER);
         frame.add(pnlMenu, BorderLayout.WEST);
@@ -105,17 +104,90 @@ public class Tool extends JFrame implements ActionListener{
         btnSave.setBounds(5, 2, 20, 1);
         btnLine = createButton("Line");
         btnLine.setBounds(5, 3, 20 ,1);
+
         btnRed = createButton("");
-        btnRed.setBackground(Color.RED);
-        btnOrange = createButton("");
-        btnOrange.setBackground(Color.ORANGE);
+        btnRed.setBackground(new Color(255, 0 ,0));
+        btnRed.setBounds(0, 0, 5, 5);
+
         btnYellow = createButton("");
-        btnYellow.setBackground(Color.YELLOW);
+        btnYellow.setBackground(new Color(255, 255, 0));
+        btnYellow.setBounds(1, 0, 5, 5);
+
+        btnGreen = createButton("");
+        btnGreen.setBackground(new Color(0, 255, 0));
+        btnGreen.setBounds(2, 0, 5, 5);
+
+        btnCyan = createButton("");
+        btnCyan.setBackground(new Color(0, 255, 255));
+        btnCyan.setBounds(3, 0, 5, 5);
+
+        btnBlue = createButton("");
+        btnBlue.setBackground(new Color(0, 0, 255));
+        btnBlue.setBounds(4, 0, 5, 5);
+
+        btnMagenta = createButton("");
+        btnMagenta.setBackground(new Color(255, 0, 255));
+        btnMagenta.setBounds(5, 0, 5, 5);
+
+        btnBlack = createButton("");
+        btnBlack.setBackground(new Color(0, 0, 0));
+        btnBlack.setBounds(6, 0, 5, 5);
+
+        btnLightGrey = createButton("");
+        btnLightGrey.setBackground(new Color(191, 191, 191));
+        btnLightGrey.setBounds(7, 1, 5, 5);
+
+        btnOrange = createButton("");
+        btnOrange.setBackground(new Color(255, 127, 0));
+        btnOrange.setBounds(0, 1, 5, 5);
+
         btnLime = createButton("");
-        btnLime.setBackground(new Color(191, 255, 64));
+        btnLime.setBackground(new Color(127, 255, 0));
+        btnLime.setBounds(1, 1, 5, 5);
+
+        btnAqua = createButton("");
+        btnAqua.setBackground(new Color(0, 255, 127));
+        btnAqua.setBounds(2, 1, 5, 5);
+
+        btnTurquoise = createButton("");
+        btnTurquoise.setBackground(new Color(0, 127, 255));
+        btnTurquoise.setBounds(3, 1, 5, 5);
+
+        btnPurple = createButton("");
+        btnPurple.setBackground(new Color(127, 0, 255));
+        btnPurple.setBounds(4, 1, 5, 5);
+
+        btnPink = createButton("");
+        btnPink.setBackground(new Color(255, 0, 127));
+        btnPink.setBounds(5, 1, 5, 5);
+
+        btnGrey = createButton("");
+        btnGrey.setBackground(new Color(127, 127, 127));
+        btnGrey.setBounds(6, 1, 5, 5);
+
+        btnWhite = createButton("");
+        btnWhite.setBackground(new Color(255, 255 ,255));
+        btnWhite.setBounds(7, 1, 5, 5);
+
         pnlMenu.add(btnLoad);
         pnlMenu.add(btnSave);
         pnlMenu.add(btnLine);
+        pnlColour.add(btnRed);
+        pnlColour.add(btnYellow);
+        pnlColour.add(btnGreen);
+        pnlColour.add(btnCyan);
+        pnlColour.add(btnBlue);
+        pnlColour.add(btnMagenta);
+        pnlColour.add(btnBlack);
+        pnlColour.add(btnLightGrey);
+        pnlColour.add(btnOrange);
+        pnlColour.add(btnLime);
+        pnlColour.add(btnAqua);
+        pnlColour.add(btnTurquoise);
+        pnlColour.add(btnPurple);
+        pnlColour.add(btnPink);
+        pnlColour.add(btnGrey);
+        pnlColour.add(btnWhite);
         frame.repaint();
         pnlMenu.repaint();
         pnlMain.repaint();
