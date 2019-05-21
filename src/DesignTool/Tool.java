8027e86e8c071e2ed4e309ee781bf3ad373ce5f8
PI\Pierce <pierceevans4@gmail.com>
2019-05-21 15:45:34 +1000

    Started GUI in Dev

--- src/DesignTool/Tool.java
+++ src/DesignTool/Tool.java
@@ -16,10 +16,28 @@ public class Tool extends JFrame implements ActionListener{
 
     private JPanel pnlMain;
     private JPanel pnlMenu;
+    private JPanel pnlColour;
 
     private JButton btnLoad;
     private JButton btnSave;
     private JButton btnLine;
+    private JButton btnRed;
+    private JButton btnOrange;
+    private JButton btnYellow;
+    private JButton btnLime;
+    private JButton btnGreen;
+    private JButton btnAqua;
+    private JButton btnCyan;
+    private JButton btnTurquoise;
+    private JButton btnBlue;
+    private JButton btnPurple;
+    private JButton btnMagenta;
+    private JButton btnPink;
+    private JButton btnBlack;
+    private JButton btnDarkGrey;
+    private JButton btnGrey;
+    private JButton btnLightGrey;
+    private JButton btnWhite;
 
     public Tool(String title){
         frame = new JFrame(title);
@@ -66,9 +84,13 @@ public class Tool extends JFrame implements ActionListener{
     public void setupPanels() {
         pnlMain = new JPanel(frame.getLayout());
         pnlMain.setBackground(Color.WHITE);
+        pnlColour = new JPanel(new GridLayout(9, 2, 5, 5));
+        pnlColour.setBackground(Color.lightGray);
+        pnlColour.setBounds(0, HEIGHT - 350, WIDTH, 350);
         pnlMenu = new JPanel(new GridLayout(10, 1, 3, 3));
         pnlMenu.setBackground(Color.RED);
         pnlMenu.setBounds(10, 150, 100, 150);
+        frame.add(pnlColour, BorderLayout.PAGE_END);
         frame.add(pnlMain, BorderLayout.CENTER);
         frame.add(pnlMenu, BorderLayout.WEST);
         frame.repaint();
@@ -83,6 +105,14 @@ public class Tool extends JFrame implements ActionListener{
         btnSave.setBounds(5, 2, 20, 1);
         btnLine = createButton("Line");
         btnLine.setBounds(5, 3, 20 ,1);
+        btnRed = createButton("");
+        btnRed.setBackground(Color.RED);
+        btnOrange = createButton("");
+        btnOrange.setBackground(Color.ORANGE);
+        btnYellow = createButton("");
+        btnYellow.setBackground(Color.YELLOW);
+        btnLime = createButton("");
+        btnLime.setBackground(new Color(191, 255, 64));
         pnlMenu.add(btnLoad);
         pnlMenu.add(btnSave);
         pnlMenu.add(btnLine);
