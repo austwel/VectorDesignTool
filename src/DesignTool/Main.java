e179ab6ff6f9b4e4d5e561461281846d17dcc2bf
Pixelply <38927893+Pixelply@users.noreply.github.com>
2019-05-31 15:35:40 +1000

    Updated Main.java and removed Tool

--- src/DesignTool/Main.java
+++ src/DesignTool/Main.java
@@ -30,6 +30,7 @@ public class Main {
     }
 
     public static void newFile() {
+        history.clear();
         System.out.println("New");
     }
 
@@ -65,7 +66,10 @@ public class Main {
         tool.draw.changePen(Color.decode(color.substring(0, 7)));
     }
 
+    public static void changePenColor(Color color) { tool.draw.changePen(color); }
+
     public static void openVec() {
+        newFile();
         JFileChooser fileChooser = new JFileChooser();
         int r = fileChooser.showOpenDialog(tool);
         if (r == JFileChooser.APPROVE_OPTION) {
